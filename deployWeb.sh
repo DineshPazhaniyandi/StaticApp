f3e2203422c87e31ed01e8f77b1fc9683935c6be
DineshPazhaniyandi <57570019+DineshPazhaniyandi@users.noreply.github.com>
2019-12-19 00:48:23 +0530

    new sh file added for deploy

---
+++ deployWeb.sh
@@ -0,0 +1,9 @@
+sudo cp /var/www/html/index.html /var/www/html/index.html_bkp
+if [ $? -eq 0 ]
+then
+    sudo rm /var/www/html/index.html
+    sudo cp -a /home/ubuntu/workspace/project_static/. /var/www/html/
+else
+    echo "Error occurred in deploying" >> /tmp/log.txt
+    exit 1
+fi
