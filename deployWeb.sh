d647c22efd4ec895e63af85ab04ed70b302ad044
DineshPazhaniyandi <57570019+DineshPazhaniyandi@users.noreply.github.com>
2019-12-19 01:02:02 +0530

    updated

--- deployWeb.sh
+++ deployWeb.sh
@@ -2,7 +2,7 @@ sudo cp /var/www/html/index.html /var/www/html/index.html_bkp
 if [ $? -eq 0 ]
 then
     sudo rm /var/www/html/index.html
-    sudo cp -a /home/ubuntu/workspace/project_static/. /var/www/html/
+    sudo cp -a /home/ubuntu/workspace/StaticHtmlProject/. /var/www/html/
 else
     echo "Error occurred in deploying" >> /tmp/log.txt
     exit 1
